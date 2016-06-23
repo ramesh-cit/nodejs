2a6c76a231d7eb28ca78bd086c406fc7e91d4b0e
ramesh-cit <ramesh.s@whitelotus.co.in>
2016-06-22 20:03:34 -0400

    Upgrade

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ var http=require("http");
     // send the HTTP header
     // HTTP status: 200:OK
     // Content Type: text/plain
-    response.writeHead(200, {'content-type':'text/plain'});
+    var callBack = function() {
+      console.log("Timeout Occurred !");
+    }
+    var bodyText = "Hello World, This is Ramesh !";
+    response.setTimeout(1000, callBack);
+    response.setHeader('X-Foo', 'bar');
+    response.writeHead(200, {
+      'Content-Length': Buffer.byteLength(bodyText),
+      'Content-Type': 'text/plain' });
 
+    console.log(request.headers);
+    response.write(bodyText) // Response Body
     // Send the response body as "Hello World"
-    response.end('Hello World\n');
+    response.end(); //close the connection
 
 }).listen(8081);
 console.log("Server running at http://127.0.0.1:8081/");
