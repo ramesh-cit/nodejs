d72f8b3834bcf9e320a29fea791b2dbdbdea8d2f
ramesh <ramesh@linux-iqt6.fios-router.home>
2016-10-23 20:37:19 -0400

    new folder for scripts :wq# Please enter the commit message for your changes. Lines starting

---
+++ todo-app/README.md
@@ -0,0 +1,7 @@
+# Start database
+mongod
+# Start Webserver (in other terminal tab)
+npm start
+# Test API (in other terminal tab)
+curl localhost:3000/todos
+# => []%
---
+++ todo-app/scripts/init.js
@@ -0,0 +1,26 @@
+console.log('Creating database for ToDo....');
+// Load mongoose package
+var mongoose = require('mongoose');
+// Connect to MongoDB and create/use database called todoAppTest
+mongoose.connect('mongodb://localhost/todoAppTest');
+console.log('Created database !');
+console.log('Creating Schema ... ')
+// Create a schema
+var TodoSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+  note: String,
+  updated_at: { type: Date, default: Date.now },
+});
+// Create a model based on the schema
+var Todo = mongoose.model('Todo', TodoSchema);
+console.log('Creating toDo Object in memory ...')
+// Create a todo in memory
+var todo = new Todo({name: 'Java Complete Reference', completed: false, note: 'Getting there...'});
+// Save it to database
+todo.save(function(err){
+  if(err)
+    console.log(err);
+  else
+    console.log(todo);
+});
